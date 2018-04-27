0ea751be8e04b2142413b11a6f91015c1bda4ea6
yekingyan <529616@gmail.com>
2018-04-27 19:00:49 +0800

    用While 0到一个数的累加
    用 raw_input 的输入的值作 while  的条件

---
+++ using 'while' for accumulating.py
@@ -0,0 +1,15 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        while
+#puporse:      1+2+3+...+
+#练习重点:     while raw_input
+# Created:     27/04/2018
+#coding:       utf-8
+#-------------------------------------------------------------------------
+
+a = 1
+s = 0
+b = int(raw_input('enter a number to caccumulate:'))
+while a < b + 1:
+    s += a
+    a += 1
+print '%d time\'s sum is %d' % (a-1 , s)
