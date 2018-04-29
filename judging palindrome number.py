080f33cc7647622894264c2169e5f7deaf7c0893
yekingyan <529616@gmail.com>
2018-04-30 01:13:25 +0800

    定义函数判断回文素数
    定义函数，使代码逻辑清晰，增强可读性

--- judging palindrome number.py
+++ judging palindrome number.py
@@ -4,7 +4,10 @@
 #coding:       utf-8
 #-----------------------------------------------------------------------
 
-def judgingprime(x):#定义一个判断质数的函数
+def judgingprime(x):#定义一个判断素数的函数
+    """
+    返回对X是否为素数的布尔值
+    """
     if x % 2 == 0 and x != 2: #除了2的偶数不为素数
         return False
     else:
@@ -14,22 +17,31 @@ def judgingprime(x):#定义一个判断质数的函数
         else:
             return True#是质数则返回True
 
-
-num = int(raw_input(u'输入一个数:'))
-t = num #储存输入值
-num_p = 0
-while num != 0:
-    num_p = num % 10 + num_p * 10#左移加上求余
-    num = num / 10 #整型,除个位数所有数右移一位,最后num变为0，结束循环
-    # print num , num_p #查看运算逻辑
-if t == num_p:
-    if judgingprime(t) == True:
-        print 'Also,%d is a prime' % t
+def judgingpalin(x):#定义一个判断回文数的函数
+    """
+    返回对X是否为回文数的布尔值
+    """
+    t = x #储存输入值
+    num_p = 0
+    while x != 0:
+        num_p = x % 10 + num_p * 10#左移加上求余
+        x = x / 10 #整型,除个位数所有数右移一位,最后num变为0，结束循环
+        # print x , num_p #查看运算逻辑
+    if t == num_p:
+        return True
     else:
-        print 'Although %d is not prime , %d is a palindrome number' % (t,t)
-elif judgingprime(t) == True:
-    print '%d is prime , %d is not a palindrome number'
-else:
-    print '%d is not a palindrome number and prime' % t
+        return False
+
+x = int(raw_input(u'输入一个数:'))
+if judgingprime(x) == True:#x为素数
+    if judgingpalin(x) == True:#同时x为回文数
+        print '%d is a prime and palindrome' % x
+    else:
+        print '%d is a prime , %d is\'t a palindrome number' % (x,x)
+else:#x不为素数
+    if judgingpalin(x) == True:#x为回文数
+        print '%d isn\'t prime , %d is a palindrome number' % (x,x)
+    else:#x不为回文数
+        print '%d is not a palindrome number and prime' % x
 
 
