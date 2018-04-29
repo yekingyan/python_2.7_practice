5afd8fac53d95e37e8463f168b19b3bd15f7a1dc
yekingyan <529616@gmail.com>
2018-04-29 19:31:12 +0800

    素数
    判断素数。找出前n个素数

--- prime.py
+++ prime.py
@@ -3,9 +3,10 @@
 # Created:     27/04/2018
 #coding:       utf-8
 #-----------------------------------------------------------------------
+'''
 #素数的判断
 x = int(raw_input(u'判断是否为素数:'))
-if x % 2 == 0:
+if x % 2 == 0 and x != 2: #除了2的偶数不为素数
     print u'%d 不是素数' % x
 else:
 
@@ -15,6 +16,29 @@ else:
             break
     else:
         print u'%d 是素数' % x
+'''
+#找出前n个素数
+import math
+times = 0
+number = 2
+n = int(raw_input(u'按顺序找到n个素数，n:'))
+while times < n:#找到n个素数停止
+    for i in range(2,int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            break
+    else:
+        print number
+        times +=1 #计数累加
+    number += 1
+
+'''
+问题
+if number % i != 0:
+    break 与 print number  交换后,无效
+             times +=1
+'''
+
+
 
 
 
