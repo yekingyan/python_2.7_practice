e64ddace6275667523e036c02d3ecca0b4c193ed
yekingyan <529616@gmail.com>
2018-04-29 23:16:02 +0800

    判断回文数与质数
    def True

--- judging palindrome number.py
+++ judging palindrome number.py
@@ -1,8 +1,20 @@
 ﻿#-------------------------------------------------------------------------------
-# Name:        回文数
+# Name:        回文数 质数
 # Created:     29/04/2018
 #coding:       utf-8
 #-----------------------------------------------------------------------
+
+def judgingprime(x):#定义一个判断质数的函数
+    if x % 2 == 0 and x != 2: #除了2的偶数不为素数
+        return False
+    else:
+        for i in (range(2,x)):
+            if x % i == 0:
+                return False
+        else:
+            return True#是质数则返回True
+
+
 num = int(raw_input(u'输入一个数:'))
 t = num #储存输入值
 num_p = 0
@@ -11,6 +23,13 @@ while num != 0:
     num = num / 10 #整型,除个位数所有数右移一位,最后num变为0，结束循环
     # print num , num_p #查看运算逻辑
 if t == num_p:
-    print '%d is a palindrome number'
+    if judgingprime(t) == True:
+        print 'Also,%d is a prime' % t
+    else:
+        print 'Although %d is not prime , %d is a palindrome number' % (t,t)
+elif judgingprime(t) == True:
+    print '%d is prime , %d is not a palindrome number'
 else:
-    print '%d is not a palindrome number'
+    print '%d is not a palindrome number and prime' % t
+
+
