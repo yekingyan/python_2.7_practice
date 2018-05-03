8d3ab74203a92613129b563080dcd3bb9dbf8a50
yekingyan <529616@gmail.com>
2018-05-03 12:59:40 +0800

    平均数 average

---
+++ average.py
@@ -0,0 +1,18 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        平均数 average
+# Created:     1/05/2018
+# coding:       utf-8
+#-------------------------------------------------------------------------------
+
+nums = []
+for i in range(10):
+    nums.append(float(raw_input('input numbers:')))
+
+s = 0
+'''
+for num in nums:
+    s += num
+avg = s / len(nums)
+'''
+avg = sum(nums) / len(nums)
+print avg
